32bbfa6670e47f35ae7862cf3a7292e8285c6ceb
malii-code <muungamimalii234@gmail.com>
2026-03-14 16:18:54 +0300

    Create DescendingNumbers.java

---
+++ DescendingNumbers.java
@@ -0,0 +1,13 @@
+//week4-task1
+//Malii Stephen Muungami
+//CT100/G/22156/24
+
+public class DescendingNumbers {
+    public static void main(String[] args) {
+        // Syntax: for (start; condition; decrement)
+        // Starts at 100, continues while i >= 50, subtracts 1 each time
+        for (int i = 100; i >= 50; i--) {
+            System.out.println(i);
+        }
+    }
+}
