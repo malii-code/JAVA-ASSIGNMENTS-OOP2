1361eda9f2ed888c1d3a0ef4ae26e81d1b7615c9
malii-code <muungamimalii234@gmail.com>
2026-03-14 15:21:47 +0300

    Create units.java

---
+++ units.java
@@ -0,0 +1,17 @@
+// Malii stephen Muungami 
+//CT100/G/22156/24
+//week1-task1
+public class units {
+    public static void main(String[] args) {
+        // Displaying the header
+        System.out.println("Units Offered This Semester:");
+        
+        // Displaying each unit on a new line
+        System.out.println("1. Object Oriented Programming 2");
+        System.out.println("2. Cloud Computing");
+        System.out.println("3. Mobile Computing");
+        System.out.println("4. Application Programming 1");
+        System.out.println("5. Object Oriented Analysis and Design");
+        System.out.println("6. Network System Administration");
+    }
+}
